dfa1ef39d96cc8f845ad80dd432460ce1e52bbfc
Shawn <dongsoso@sina.com>
2015-04-28 19:01:35 +0800

    1

---
+++ public/javascripts/home.js
@@ -0,0 +1,17 @@
+requirejs.config({
+    baseUrl: '../javascripts',
+    paths: {
+        'jquery': 'libs/jquery',
+        'panelslider': 'libs/jquery.panelslider.min',
+        'eventproxy': 'libs/eventproxy',
+        'bootstrap': '../stylesheets/bootstrap/js/bootstrap.min'
+    }
+});
+
+// Panel slider
+require(['jquery', 'panelslider', 'bootstrap'], function ($, panelslider) {
+	$('#left-panel-link').panelslider();
+    $('#close-panel-bt').click(function() {
+      	$.panelslider.close();
+    });
+});
